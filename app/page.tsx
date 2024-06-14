1bff088d663969427b627311f998e163852577ed
Hafiz-fish <hafizmrifqi@gmail.com>
2024-06-15 01:18:15 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="bg-gray-100 flex items-center justify-center h-screen">
       <div className="text-center">
         <h1 className="text-9xl font-bold text-gray-800 mb-4">401</h1>
-        <p className="text-2xl md:text-3xl text-gray-600 mb-8">Oops! You do not have permission to view this page.</p>
+        <p className="text-2xl md:text-3xl text-gray-600 mb-8">Oops! We do not have permission to access the camera. Please allow access when open this verivication</p>
         <a href="#" className="px-6 py-3 bg-blue-400 text-white rounded-full hover:bg-blue-700 transition duration-300">
           Refresh
         </a>
